553db705e5b450b44eb9142a917922278656d002
Zach Shirah <zachshirah01@gmail.com>
2025-01-26 00:59:25 -0500

    trying to figure out how to get the toggle and the ws decoupled

--- push_the_goddamn_button/main.py
+++ push_the_goddamn_button/main.py
@@ -17,17 +17,21 @@ def update_image(pg13):
 
     return Div(Img(src=img_name, hx_swap_oob='true', id='image'))
 
+def update_count(): 
+    return P(count.value, id='count')
+    
 def update_counter_txt(pg13):
-    return P(f"The{' goddamn' if pg13 else ''} button has been pushed {count.value}x", hx_swap_oob='true', id='count')
+    return P(f"Number of{' goddamn' if pg13 else ''} button pushes: ", hx_swap_oob='true', id='counter_txt')
 
 def update_title(pg13):
     return H1(f'PUSH THE{" GODDAMN" if pg13 else ""} BUTTON!', id='title')
+
         
 @rt('/')
 def get():
     pg13_toggle = Label(
         "I want to see the PG-13 version", 
-        Input(type='checkbox', role='switch', hx_get='/pg13', target_id='title')
+        Input(type='checkbox', role='switch', hx_get='/pg13')
     )
 
     button = Div(Svg(w=50, h=50)(
@@ -41,28 +45,26 @@ def get():
         ws_connect='/ws'
     )
     
-    title = f'PUSH THE{" GODDAMN" if pg13 else ""} BUTTON!'
+    button_push_txt = update_counter_txt(pg13)
 
-    button_presses = update_counter_txt(pg13)
+    button_push_ct = update_count()
+
+    button_push = Div(button_push_txt, button_push_ct)
 
     title_h = update_title(pg13)
 
-    return Title(title), Main(title_h, pg13_toggle, update_image(pg13), button, button_presses, cls='main')
+    return Title("PUSH THE BUTTON"), Main(title_h, pg13_toggle, update_image(pg13), button, button_push)
 
 users = {}
 def on_conn(ws, send): users[str(id(ws))] = send
 def on_disconn(ws): users.pop(str(id(ws)), None)
-
-async def update_count():
-    global pg13
-    for u in users.values(): 
-        await u(update_counter_txt(pg13))
-
 @app.ws('/ws', conn=on_conn, disconn=on_disconn)
 async def ws(msg:str, send): 
+    global pg13
     count.value = count.value + 1
     counts.update(count)
-    await update_count()
+    for u in users.values(): 
+        await u(update_count())
 
 @rt('/pg13')
 def get():
