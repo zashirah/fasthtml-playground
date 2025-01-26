ea75ad116c24a4453f08a1e1c8978b0f47e7e7b9
Zach Shirah <zachshirah01@gmail.com>
2025-01-26 09:53:44 -0500

    trying to figure out how to get the toggle and the ws decoupled

--- push_the_goddamn_button/main.py
+++ push_the_goddamn_button/main.py
@@ -11,22 +11,14 @@ except: counts.insert(Count(value=0))
 count = counts[1]
 pg13 = False
 
-def update_image(pg13):
-    if pg13: img_name = 'pushthegdbutton.jpeg'
-    else: img_name = 'pushthebutton.jpeg'
+def update_image(pg13): return Div(Img(src=f'pushthe{"gd" if pg13 else ""}button.jpeg'), hx_swap_oob='true', id='image')
 
-    return Div(Img(src=img_name, hx_swap_oob='true', id='image'))
-
-def update_count(): 
-    return P(count.value, id='count')
+def update_count(): return P(count.value, id='count')
     
-def update_counter_txt(pg13):
-    return P(f"Number of{' goddamn' if pg13 else ''} button pushes: ", hx_swap_oob='true', id='counter_txt')
+def update_counter_txt(pg13): return P(f"Number of{' goddamn' if pg13 else ''} button pushes: ", hx_swap_oob='true', id='counter_txt')
 
-def update_title(pg13):
-    return H1(f'PUSH THE{" GODDAMN" if pg13 else ""} BUTTON!', id='title')
+def update_title(pg13): return H1(f'PUSH THE{" GODDAMN" if pg13 else ""} BUTTON!', hx_swap_oob='true', id='title')
 
-        
 @rt('/')
 def get():
     pg13_toggle = Label(
@@ -34,13 +26,15 @@ def get():
         Input(type='checkbox', role='switch', hx_get='/pg13')
     )
 
-    button = Div(Svg(w=50, h=50)(
+    button = Div(
+        Svg(w=50, h=50)(
         Circle(
             20, 25, 25, 
             stroke='red', 
             stroke_width=3,
             id='goddamn_btn',
-            ws_send=True)),
+            ws_send=True)
+        ),
         hx_ext='ws',
         ws_connect='/ws'
     )
@@ -60,7 +54,6 @@ def on_conn(ws, send): users[str(id(ws))] = send
 def on_disconn(ws): users.pop(str(id(ws)), None)
 @app.ws('/ws', conn=on_conn, disconn=on_disconn)
 async def ws(msg:str, send): 
-    global pg13
     count.value = count.value + 1
     counts.update(count)
     for u in users.values(): 
@@ -72,5 +65,4 @@ def get():
     pg13 = not pg13
     return update_title(pg13), update_image(pg13), update_counter_txt(pg13)
 
-if __name__ == '__main__':
-    serve()
+if __name__ == '__main__': serve()
