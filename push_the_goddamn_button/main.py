fe0e0c0c03c3444d225ec826ba95cf45474a2f4f
Zach Shirah <zachshirah01@gmail.com>
2025-01-25 23:22:36 -0500

    got the gd button working with websockets + added a websockets demo project

--- push_the_goddamn_button/main.py
+++ push_the_goddamn_button/main.py
@@ -2,25 +2,29 @@ from fasthtml.common import *
 from fasthtml.svg import *
 import uvicorn
 
+if __name__ == "__main__": sys.exit("Run this app with `uvicorn main:app`")
+
 app,rt,counts,Count = fast_app('data/counts.db', live=True, 
     hdrs=[Style(':root { --pico-font-size: 100%; }')],
-    id=int, value=int, pk='id')
+    id=int, value=int, pk='id',
+    exts='ws'
+)
+try: counts[1]
+except: counts.insert(Count(value=0))
+count = counts[1]
 
 @rt('/')
 def get():
-    try: counts[1]
-    except: counts.insert(Count(value=0))
-    
-    count = counts[1]
-
-    button = Svg(w=50, h=50)(
+    button = Div(Svg(w=50, h=50)(
         Circle(
             20, 25, 25, 
             stroke='red', 
             stroke_width=3,
             id='goddamn_btn',
-            hx_get='/increment/1',
-            target_id='count'))
+            ws_send=True)),
+        hx_ext='ws',
+        ws_connect='/ws'
+    )
 
     title = 'PUSH THE GODDAMN BUTTON!'
 
@@ -29,14 +33,16 @@ def get():
     button_presses = P(f'The goddamn button has been pushed {count.value}x', id='count')
     return Title(title), Main(H1(title), img, button, button_presses)
 
-@rt('/increment/{cid}')
-def get(cid:int):
-    count = counts[cid]
+users = {}
+def on_conn(ws, send): users[str(id(ws))] = send
+def on_disconn(ws): users.pop(str(id(ws)), None)
+
+async def update_count():
+    for u in users.values(): 
+        await u(P(f'The goddamn button has been pushed {count.value}x', id='count'))
+
+@app.ws('/ws', conn=on_conn, disconn=on_disconn)
+async def ws(msg:str, send): 
     count.value = count.value + 1
     counts.update(count)
-    return P(f'The goddamn button has been pushed {count.value}x')
-
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", default=8000)))
-
-serve()
+    await update_count()
